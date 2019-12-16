ace68f1605ff5af74bf813914446379596cca6d3
Dmytro Semenov <dimichsdg@yahoo.com>
2019-12-15 22:14:27 -0800

    Create example.js

---
+++ example.js
@@ -0,0 +1,30 @@
+'use strict';
+/******* Basic ********/
+const request = require('request');
+/*
+ * Calculates square root of sum of a and b
+**/
+module.exports = async (req, res) => {
+  const { a, b } = req.query;
+  const sum = await new Promise((resolve, reject) => {
+    request(`http://service.xyz/sum?a=${a}&b=${b}`, (err, response) => {
+      if (err) {
+        return reject(err);
+      }
+      return response.body;
+    });
+  });
+  
+  const sqr = await new Promise((resolve, reject) => {
+    request('http://service.xyz/sqr?value=${sum}', (err, response) => {
+      if (err) {
+        return reject(err);
+      }
+      return response.body;
+    });
+  });
+
+  res.json({
+    result: sqr
+  });
+};
